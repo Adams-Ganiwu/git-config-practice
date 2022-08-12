c446b0108c9ca45dabcfe1644f00b8301b54731b
Adams-Ganiwu <ibrahimabdulatif1@gmail.com>
2022-08-12 11:47:51 +0000

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+          <h1>Welcome to Gamson Hood</h1>
+          <nav>
+              <div>
+                  <ul>
+                      
+                      <li>Home</li>
+                      <li>About</li>
+                      <li>Product</li>
+                      <li>Contact</li>
+
+                  </ul>
+              </div>
+          </nav>
+    </div>
+  )
+}
+
+export default NavBar
